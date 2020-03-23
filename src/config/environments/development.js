eb6fac532a3659af23675fa9e2c9a7f3ee7f8f03
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-03-24 00:14:51 +0100

    project-setup

---
+++ src/config/environments/development.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: "127.0.0.1",
+    port: 5000, // change with development port
+    mongoUrl: "mongodb://localhost:27017/jungle", // replace "projectDbName" with a proper db name
+    logLevel: "debug", // can be chenged to error, warning, info, verbose or silly
+    secret: "superSuperSecret",
+};
