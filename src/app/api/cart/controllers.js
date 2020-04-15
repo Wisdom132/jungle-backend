ffefe7dc7a8674626dfc84c82aa410108862bea1
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-04-15 23:53:02 +0100

    cart comment

--- src/app/api/cart/controllers.js
+++ src/app/api/cart/controllers.js
@@ -1,9 +1,11 @@
 exports.addItemToCart = async () => {
+    //add new item to cart
 };
 exports.removeItem = async () => {
+    //remove item from cart
 };
-exports.emptyCart = async () => {
-}
+
+exports.emptyCart = async () => {}
 
 exports.preceedCheckout = async () => {
 
