6fa6cb26ef9c9bb2ea7da52891e61496c423a526
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-05-08 21:20:51 +0100

    feature to subtract cart item quantity

--- src/app/api/cart/controllers.js
+++ src/app/api/cart/controllers.js
@@ -93,6 +93,64 @@ exports.addItemToCart = async (req, res) => {
         })
     }
 }
+exports.subtractItem = async (req, res) => {
+    const {
+        userId,
+        productId
+    } = req.body;
+    const quantity = Number.parseInt(req.body.quantity);
+    try {
+        // -------Get users Cart ------
+        let cart = await Cart.findOne({
+            userId: userId
+        })
+        //-----Get Selected Product Details ----
+        const productDetails = await Product.findById(productId);
+        //-- Check if cart Exists and Check the quantity if items -------
+        if (!cart && quantity <= 0) {
+            return res.status(400).json({
+                type: "Invalid",
+                msg: "Invalid request"
+            })
+        }
+        if (cart) {
+            //---- check if index exists
+            const indexFound = cart.items.findIndex(item => item.productId == productId);
+            //----------check if product exist,just add the previous quantity with the new quantity and update the total price-------
+            if (indexFound !== -1) {
+                let updatedQuantity = cart.items[indexFound].quantity = cart.items[indexFound].quantity - quantity;
+                //-----Check if the updated Quantity is less than or equal to 0,Then remove that item from the items array and then update the sumTotal value
+                if (updatedQuantity <= 0) {
+                    console.log("wevk")
+                    cart.items.splice(indexFound, 1);
+                    if (cart.items.length == 0) {
+                        cart.subTotal = 0;
+
+                    } else {
+                        cart.subTotal = cart.items.map(item => item.total).reduce((acc, next) => acc + next);
+
+                    }
+                } else {
+                    cart.items[indexFound].quantity = updatedQuantity;
+                    cart.items[indexFound].total = cart.items[indexFound].quantity * productDetails.price;
+                    cart.items[indexFound].price = productDetails.price
+                    cart.subTotal = cart.items.map(item => item.total).reduce((acc, next) => acc + next);
+                }
+
+            }
+
+            let data = await cart.save();
+            res.status(200).json({
+                type: "success",
+                mgs: "Process Successful",
+                data: data
+            })
+        }
+
+    } catch (err) {
+
+    }
+}
 exports.getUserCart = async (req, res) => {
     try {
         let user = req.query.user
--- src/app/api/cart/routes.js
+++ src/app/api/cart/routes.js
@@ -2,6 +2,7 @@ const cartController = require("./controllers");
 const router = require("express").Router();
 
 router.post("/add-item-to-cart", cartController.addItemToCart);
+router.post("/subtract-item-to-cart", cartController.subtractItem);
 router.get("/get-cart", cartController.getUserCart);
 
 module.exports = router
